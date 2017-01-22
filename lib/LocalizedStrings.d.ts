1dd2d186e35ea2d7260b8bf10013021c65f80601
Benjamin Rau <benngiro@gmail.com>
2017-01-22 21:57:24 +0100

    fix(typings): Fix export style
    Don't use commonjs / amd style because then import needs to be of style 'import let =
    
    require("module")' which is not compatible with es6 module exports. (see
    
    https://www.typescriptlang.org/docs/handbook/modules.html)

--- lib/LocalizedStrings.d.ts
+++ lib/LocalizedStrings.d.ts
@@ -1,17 +1,14 @@
-declare class LocalizedStrings {
-  // Indexer
-  [key: string]: string | ( (...args: any[]) => any );
-
-  constructor(props: { [language: string]: { [key: string]: string }});
-  getLanguage(): string;
-  setLanguage(language: string): void;
-  getInterfaceLanguage(): string;
-  getAvailableLanguages(): string[];
-  formatString(str: string, ...values: string[]): string;
-  getString(key: string, language: string): string;
-}
-
 declare module 'react-native-localization' {
-	export = LocalizedStrings;
-}
+    export class LocalizedStrings {
+        // Indexer
+        [key: string]: string | ((...args: any[]) => any);
 
+        constructor(props: { [language: string]: { [key: string]: string } });
+        getLanguage(): string;
+        setLanguage(language: string): void;
+        getInterfaceLanguage(): string;
+        getAvailableLanguages(): string[];
+        formatString(str: string, ...values: string[]): string;
+        getString(key: string, language: string): string;
+    };
+}
